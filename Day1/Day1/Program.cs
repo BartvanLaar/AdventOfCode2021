17cb192c806d69d9a1f486025eb80e909529fe10
Bart van Laar <b.vanlaar@bronkhorst.com>
2021-12-01 11:20:53 +0100

    aded solution to challenge 2

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -25,6 +25,23 @@
 
         private static void ChallengeTwo()
         {
+            const string INPUT_FILE_NAME = "InputDataChallenge2.txt";
+            var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
+            var data = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries).Select(d => int.Parse(d)).ToArray();
+            Console.WriteLine("How many rolling window (3 wide) measurements are larger than the previous rolling window (3 wide) measurement?");
+            var result = -1; // first one doesn't count, so skip.
+            var prevValue = 0;
+            for (var i = 2; i < data.Length; i++)
+            {
+                var newValue = data[i] + data[i - 1] + data[i - 2];
+                if (newValue > prevValue)
+                {
+                    result++;
+                }
+                prevValue = newValue;
+            }
+
+            Console.WriteLine($"Answer is: {result}.");
 
         }
 
