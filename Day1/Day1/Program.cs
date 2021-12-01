15a03c83181eac04f999d15c2e604054d38ee85f
Bart van Laar <b.vanlaar@bronkhorst.com>
2021-12-01 10:49:51 +0100

    implemented challenge 1

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -2,9 +2,32 @@
 {
     public class Program
     {
+
+
         public static void Main(params string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world, today there are 2 challenges!");
+
+            ChallengeOne();
+            ChallengeTwo();
         }
+
+        public static void ChallengeOne()
+        {
+            const string INPUT_FILE_NAME = "InputDataChallenge1.txt";
+            var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
+            var data = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+
+            Console.WriteLine("How many measurements are larger than the previous measurement?");
+            var result = data.Skip(1).Where((d, i) => int.Parse(d) > int.Parse(data[i])).Count();
+            Console.WriteLine($"Answer is: {result}.");
+        }
+
+        private static void ChallengeTwo()
+        {
+
+        }
+
+
     }
 }
