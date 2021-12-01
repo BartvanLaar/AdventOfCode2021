350adb0c7c7d7a6dbb7c1c971112cde4b79f43cb
Bart van Laar <b.vanlaar@bronkhorst.com>
2021-12-01 10:21:52 +0100

    added day1 hello world initial commit

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day1
+{
+    public class Program
+    {
+        public static void Main(params string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
