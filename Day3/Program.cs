ff713b77b34933c2931c6ada9e979e135a4994fa
bart <bartvanlaar1997@gmail.com>
2021-12-05 20:37:28 +0100

    implemented challenge 1 and 2 of day 3

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -14,19 +14,21 @@
         {
             const string INPUT_FILE_NAME = "InputDataChallenge1.txt";
             var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
-            var data = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+            var stringData = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+            var maxLength = stringData.Max(x => x.Length);
+            var data = stringData.Select(x => Convert.ToInt32(x, 2)).ToArray();
 
-            var gammaRateAsString = string.Empty;
-            var epsilonRateAsString = string.Empty;
-            for (var i = 0; i < data.Max(x => x.Length); i++)
-            {
-                var maxBitOccurrence = data.Select(x => x[i]).GroupBy(x => x).Select(x => (Bit: x.First(), Count: x.Count())).MaxBy(x => x.Count);
-                gammaRateAsString += maxBitOccurrence.Bit;
-                epsilonRateAsString += maxBitOccurrence.Bit == '1' ? '0' : '1';
-            }
-            var gammaRate = Convert.ToInt32(gammaRateAsString, 2);
-            var epsilonRate = Convert.ToInt32(epsilonRateAsString, 2);
-
+            var gammaRateAsArrayOfBits = Enumerable.Range(0, maxLength)
+                .Reverse()
+                .Select(index => data
+                    .Select(n => (n >> index) & 1)
+                    .GroupBy(x => x)
+                    .Select(y => (Bit: y.First(), Count: y.Count()))
+                    .MaxBy(z => z.Count))
+                .Select(x => x.Bit);
+            var gammaRate = Convert.ToInt32(string.Join(string.Empty, gammaRateAsArrayOfBits), 2);
+            var mask = (1 << maxLength) - 1; // set mask to be able to get N required bits.... n= maxLength
+            var epsilonRate = ~gammaRate & mask; // mask int to get N required bits 
             var powerConsumption = gammaRate * epsilonRate;
 
             Console.WriteLine($"Power consumption of submarine is {powerConsumption}");
@@ -35,9 +37,59 @@
 
         private static void ChallengeTwo()
         {
-          
-        }
+            const string INPUT_FILE_NAME = "InputDataChallenge2.txt";
+            var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
+            var stringData = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+            var maxLength = stringData.Max(x => x.Length);
+            IEnumerable<int> oxygenRatingData = stringData.Select(x => Convert.ToInt32(x, 2)).ToArray();
+            IEnumerable<int> co2ScrubberRatingData = stringData.Select(x => Convert.ToInt32(x, 2)).ToArray();
 
+            foreach (var index in Enumerable.Range(0, maxLength).Reverse())
+            {
+                var temp = oxygenRatingData.Select(n => (n >> index) & 1).GroupBy(x => x).Select(y => (Bit: y.First(), Count: y.Count()));
+
+                if (temp.Select(x => x.Count).Distinct().Count() == 1)
+                {
+                    oxygenRatingData = oxygenRatingData.Where(n => ((n >> index) & 1) == 0b1);
+                }
+                else
+                {
+                    var selectionFilter = temp.MaxBy(x => x.Count).Bit;
+                    oxygenRatingData = oxygenRatingData.Where(n => ((n >> index) & 1) == selectionFilter);
+                }
+
+                if (oxygenRatingData.Count() < 2)
+                {
+                    break;
+                }
+            }
+
+            var oxygenGeneratorRating = oxygenRatingData.First();
+
+            foreach (var index in Enumerable.Range(0, maxLength).Reverse())
+            {
+                var temp = co2ScrubberRatingData.Select(n => (n >> index) & 1).GroupBy(x => x).Select(y => (Bit: y.First(), Count: y.Count()));
+
+                if (temp.Select(x => x.Count).Distinct().Count() == 1)
+                {
+                    co2ScrubberRatingData = co2ScrubberRatingData.Where(n => ((n >> index) & 1) == 0b0);
+                }
+                else
+                {
+                    var selectionFilter = temp.MinBy(x => x.Count).Bit;
+                    co2ScrubberRatingData = co2ScrubberRatingData.Where(n => ((n >> index) & 1) == selectionFilter);
+                }
+
+                if (co2ScrubberRatingData.Count() < 2)
+                {
+                    break;
+                }
+            }
+
+            var co2ScrubberRating = co2ScrubberRatingData.First();
+
+            Console.WriteLine($"The life support rating of the submarine is {oxygenGeneratorRating * co2ScrubberRating}");
+        }
 
     }
 }
