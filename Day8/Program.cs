4016a41a1e37381f7ca397780efb0702c1761ec1
Bart van Laar <b.vanlaar@bronkhorst.com>
2021-12-08 08:29:37 +0100

    added empty day 8 project

---
+++ Day8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace Day8
+{
+    public class Program
+    {
+        public static void Main(params string[] args)
+        {
+            Console.WriteLine("Hello world, today there are 2 challenges!");
+
+            ChallengeOne();
+            ChallengeTwo();
+        }
+
+        public static void ChallengeOne()
+        {
+            var sw = new Stopwatch();
+            const string INPUT_FILE_NAME = "InputDataChallenge2.txt";
+            var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
+            IEnumerable<string> stringData = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+            IEnumerable<int> intData = stringData.SelectMany(s => s.Split(",").Select(i => int.Parse(i)));
+            sw.Start();
+
+
+
+            sw.Stop();
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds} ms after reading in the start data.");
+        }
+
+        private static void ChallengeTwo()
+        {
+            var sw = new Stopwatch();
+            const string INPUT_FILE_NAME = "InputDataChallenge2.txt";
+            var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
+            IEnumerable<string> stringData = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+            IEnumerable<int> intData = stringData.SelectMany(s => s.Split(",").Select(i => int.Parse(i)));
+            sw.Start();
+
+
+            sw.Stop();
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds} ms after reading in the start data.");
+        }
+
+
+
+    }
+}
