489e34b07894cb184372eda7ba75e195ac7093c7
Bart van Laar <b.vanlaar@bronkhorst.com>
2021-12-09 09:49:38 +0100

    added day9 project.

---
+++ Day9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace Day9
+{
+    public class Program
+    {
+        public static void Main(params string[] args)
+        {
+            Console.WriteLine("Hello world, today there are 2 challenges!");
+
+            ChallengeOne();
+            ChallengeTwo();
+        }
+
+        public static void ChallengeOne()
+        {
+            var sw = new Stopwatch();
+            const string INPUT_FILE_NAME = "InputDataChallenge2.txt";
+            var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
+            IEnumerable<string> stringData = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+            IEnumerable<int> intData = stringData.SelectMany(s => s.Split(",").Select(i => int.Parse(i)));
+            sw.Start();
+
+
+
+
+            sw.Stop();
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds} ms after reading in the start data.");
+        }
+
+        private static void ChallengeTwo()
+        {
+            var sw = new Stopwatch();
+            const string INPUT_FILE_NAME = "InputDataChallenge2.txt";
+            var inputData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), INPUT_FILE_NAME));
+            IEnumerable<string> stringData = inputData.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+            const char ZERO = '0';
+            int[][] intData = stringData.Select(s => s.Select(n => n - ZERO).ToArray()).ToArray();
+            sw.Start();
+
+
+            for(var y = 0; y < intData.Length; y++)
+            {
+                for(var x = 0; x < intData[y].Length; x ++)
+                {
+
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds} ms after reading in the start data.");
+        }
+
+
+
+    }
+}
